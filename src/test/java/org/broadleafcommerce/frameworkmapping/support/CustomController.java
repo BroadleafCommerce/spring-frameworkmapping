7185038808f7fb8703fd431a9a56cb19dd39da81
Phillip Verheyden <pverheyden@gmail.com>
2018-08-29 20:03:51 -0500

    Remove warning

--- src/test/java/org/broadleafcommerce/frameworkmapping/support/CustomController.java
+++ src/test/java/org/broadleafcommerce/frameworkmapping/support/CustomController.java
@@ -1,8 +1,6 @@
 package org.broadleafcommerce.frameworkmapping.support;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomController {
 
     /*
-        Overrides the default endpoint
+     * Overrides the default endpoint
      */
     @GetMapping("/overridden-get")
     public String overridingGetEndpoint() {
